4c61a7d0abc364d5f6f48c8fb4f1e3e4184a64f4
Jan Antolik <antolikjan@gmail.com>
2011-10-18 15:13:29 +0200

    added new plotting files

---
+++ visualization/jens_paper_plots.py
@@ -0,0 +1,20 @@
+import MozaikLite.storage.queries as queries
+from MozaikLite.visualization.plotting import Plotting,RasterPlot,GSynPlot,VmPlot,ConductanceSignalListPlot 
+from NeuroTools.parameters import ParameterSet, ParameterDist
+import matplotlib.gridspec as gridspec
+from MozaikLite.storage.queries import select_stimuli_type_query,select_result_sheet_query, partition_by_stimulus_paramter_query
+
+class Figure2(Plotting):
+      required_parameters = ParameterSet({
+            'sheet_name' : str,  #the name of the sheet for which to plot
+      })
+      
+      def subplot(self,subplotspec):
+          gs = gridspec.GridSpecFromSubplotSpec(12, 8, subplot_spec=subplotspec,hspace=1.0,wspace=1.0)  
+          
+          dsv = queries.select_stimuli_type_query(self.datastore,'FullfieldDriftingSinusoidalGrating',['*','*','*','*',90.0,'*','*','*',0.0,'*','*'])
+          RasterPlot(dsv,ParameterSet({'sheet_name' : self.parameters.sheet_name, 'trial_averaged_histogram' : True})).subplot(gs[0:4,:5])
+          GSynPlot(dsv,ParameterSet({'sheet_name' : self.parameters.sheet_name,'neuron' : 0})).subplot(gs[4:8,:5])
+          VmPlot(dsv,ParameterSet({'sheet_name' : self.parameters.sheet_name,'neuron' : 0})).subplot(gs[8:12,:5])          
+          
+          ConductanceSignalListPlot(queries.TagBasedQuery(ParameterSet({'tags' : ['GSTA1'] })).query(self.datastore),ParameterSet({'sheet_name' : 'V1_Exc'})).subplot(gs[2:6,5:])  
---
+++ visualization/plotting_helper_functions.py
@@ -0,0 +1,41 @@
+import pylab
+
+def disable_top_right_axis(ax):
+    for loc, spine in ax.spines.iteritems():
+            if loc in ['right','top']:
+               spine.set_color('none') # don't draw spine
+    for tick in ax.yaxis.get_major_ticks():
+        tick.tick2On = False
+    for tick in ax.xaxis.get_major_ticks():
+        tick.tick2On = False
+
+def three_tick_axis(axis):
+    nticks = len([t for t in axis.get_major_ticks()])
+    print 'DASDASDASDASDASDAS'
+    print [t for t in axis.get_major_ticks()]
+    print nticks
+    if (nticks % 2) != 0:
+       middle_tick = int(nticks) / 2  
+    else:
+       middle_tick = 0
+       
+    for i,tick in enumerate(axis.get_major_ticks()):
+        print tick.label1
+        if i != 0 and i != (nticks-1) and i != middle_tick:
+           tick.tick2On = False     
+           tick.tick1On = False     
+           tick.label2On = False     
+           tick.label1On = False     
+    
+    
+def disable_xticks(ax):
+    for t in ax.xaxis.get_ticklines(): t.set_visible(False)
+    
+def disable_yticks(ax):
+    for t in ax.yaxis.get_ticklines(): t.set_visible(False)
+
+def remove_x_tick_labels():
+    pylab.xticks([],[])  
+ 
+def remove_y_tick_labels(): 
+    pylab.yticks([],[])  
