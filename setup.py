f47b841a52b3b4b3d8ca90009840042f898dd148
Jan Antolik <antolikjan@gmail.com>
2012-03-07 16:27:20 +0100

    doing packaging

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from distutils.core import setup
+      
+setup(
+    name = "mozaik",
+    version = "0.1dev",
+    package_dir={'mozaik': 'mozaik'},
+    packages = ['mozaik',
+                'mozaik.analysis',
+                'mozaik.framework',
+                'mozaik.models',
+                'mozaik.models.retinal',
+                'mozaik.stimuli',
+                'mozaik.storage',
+                'mozaik.tools',
+                'mozaik.visualization'
+                ],
+    author = "The mozaik team",
+    author_email = "antolikjan@gmail.com",
+    description = "Python package mozaik is a integrated workflow framework for large scale neural simulations.",
+    license = "CeCILL http://www.cecill.info",
+    keywords = "computational neuroscience simulation large-scale model spiking",
+    url = "",
+    classifiers = ['Development Status :: 2 - Pre-Alpha',
+                   'Environment :: Console',
+                   'Intended Audience :: Science/Research',
+                   'License :: Other/Proprietary License',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering'],
+    cmdclass = {'build': build},
+)
+
