fe170ac764eeff71faed0486fa4e2fd06376f8b0
Jan Antolik <antolikjan@gmail.com>
2012-08-02 16:41:16 +0200

    forgot to add new files

---
+++ mozaik/stimuli/stimulus.py
@@ -0,0 +1,327 @@
+"""
+This package defines the API for:
+    - implementation of stimuli as input to models (see class Stimulus)
+    - identification of stimulus identity (without the actual 'data' of the stimulus) throughout *mozaik* (see class StimulusID)
+      (NOTE: throughout most mozaik (particularly all post processing i.e. analysis and plotting) user will interact with StimulusIDs rather than Stimulus instances!!)
+    - function helpers for common manipulation with collections of stimuli (or rather their IDs)
+
+Each stimulus is expected to have a dictionary of parameters which have to uniquely identify the stimulus.
+We implement this by using the Parametrized package that allows to specify parameters with the all above requirements.
+For Stimuli objects we will allow only SNumber, SInteger and SString parameters. These extend the
+corresponding parametrized parameters to allow specification of units (see tools/mozaik_parametrized.py).
+
+Note that *all* such parameters defined in the class (and its ancestors) will be considered as parameters of the Stimulus.
+"""
+
+import quantities as qt
+import numpy 
+from operator import * # don't do import *
+from mozaik.tools.mozaik_parametrized import * # don't do import *
+from mozaik.framework.interfaces import MozaikParametrizeObject
+import inspect
+from NeuroTools.parameters import ParameterSet
+
+class Stimulus(MozaikParametrized):
+    """
+    Abstract class.
+    """
+    frame_duration = SNumber(qt.ms,doc="""The duration of single frame""")
+    duration = SNumber(qt.ms,doc="""The duration of stimulus""")
+    trial = SInteger(doc="""The trial of the stimulus""")
+
+    def __init__(self, **params):
+        MozaikParametrized.__init__(self,**params)
+        self.input = None
+        self._frames = self.frames()
+        self.n_frames = numpy.inf # possibly very dangerous. Don't do 'for i in range(stim.n_frames)'!
+        self.update()
+
+    def __str__(self):
+        return str(StimulusID(self))
+
+    def __eq__(self, other):
+        return self.equalParams(other) and (self.__class__ == other.__class__)
+
+    def frames(self):
+        """
+        Return a generator which yields the frames of the stimulus in sequence.
+        Each frame is returned as a tuple `(frame, variables)` where
+        `frame` is a numpy array containing the stimulus at the given time and `variables` is a list of
+        variable values (e.g., orientation) associated with that frame.
+
+        See topographica_based for examples.
+        """
+        raise NotImplementedError("Must be implemented by child class.")
+
+    def update(self):
+        """
+        Sets the current frame to the next frame in the sequence.
+        """
+        raise NotImplementedError("Must be implemented by child class.")
+
+    def reset(self):
+        """
+        Reset to the first frame in the sequence.
+        """
+        raise NotImplementedError("Must be implemented by child class.")
+
+    def export(self, path=None):
+        """
+        Save the frames to disk. Returns a list of paths to the individual
+        frames.
+        
+        path - the directory in which the individual frames will be saved. If
+               path is None, then a temporary directory is created.
+        """
+        raise NotImplementedError("Must be implemented by child class.")
+        
+
+class StimulusID():
+      """
+      StimulusID is a light object that contains all the parameter info of a given Stimulus object.
+      There is one to one mapping between StimuluID instances and Stimulus instances.
+      
+      The main purpose for StimulusID is to stand for Stimulus whenever the stimulus identitity is required
+      without the need to move around the heavy Stimulus objects.
+      
+      Unlike Stimulus, StimulusID allows any parameter to be also assigned None. This is often 
+      used in data analysis to mark parameters that has been 'computed out' by the analysis (such as averaged out).
+      
+      To access the parameter values refer to the param member variable
+      To access the units values refer to the units member variable
+      To access the periodicity (or lack of it) values refer to the periods member variable
+      """  
+
+      def __str__(self):
+            """
+            Saves the parameter names and values as a dict
+            """
+            settings = ['\"%s\":%s' % (name,repr(val))
+                    for name,val in self.get_param_values()]
+            
+            r= "{ \"name\" :" + "\"" + self.name + "\""+ "," + "\"module_path\" :" + "\"" + self.module_path + "\"" +',' + ", ".join(settings) + "}"
+            return r
+
+      def __eq__(self, other):
+            return (self.name == other.name) and (self.get_param_values() == other.get_param_values())
+            
+      def get_param_values(self):
+          z = self.params.items()
+          z.sort(key=itemgetter(0))
+          return z
+          
+      def number_of_parameters(self):
+          return len(self.params.keys())
+          
+      def load_stimulus(self):
+          cls = self.getStimulusClass()
+          return cls(**self.params)  
+      
+      def getStimulusClass(self):
+          z = __import__(self.module_path,globals(),locals(),self.name)
+          return  getattr(z,self.name)
+          
+      def copy(self):
+          return StimulusID(str(self))
+          
+      def __init__(self,obj):
+          self.units = {}
+          self.periods = {}
+          self.params = {}
+          if isinstance(obj,Stimulus):
+              self.name = obj.__class__.__name__
+              self.module_path = inspect.getmodule(obj).__name__
+              par = obj.params()
+              for n,v in obj.get_param_values():
+                  if n != 'name' and n != 'print_level':
+                      self.params[n] = v
+                      self.units[n] = par[n].units
+                      self.periods[n] = par[n].period
+          elif isinstance(obj,dict):
+              self.name = obj.pop("name")
+              self.module_path = obj.pop("module_path")
+              par = self.getStimulusClass().params()
+              for n,v in obj.items():
+                  self.params[n] = v
+                  self.units[n] = par[n].units
+                  self.periods[n] = par[n].period                  
+          elif isinstance(obj,str):
+               d = eval(obj)
+               self.name = d.pop("name")
+               self.module_path = d.pop("module_path")
+               par = self.getStimulusClass().params()
+               for n,v in d.items():
+                  self.params[n] = v
+                  self.units[n] = par[n].units
+                  self.periods[n] = par[n].period
+          elif isinstance(obj,StimulusID):
+               self.name = obj.name
+               self.module_path = obj.module_path
+               self.units = obj.units.copy()
+               self.periods = obj.periods.copy()
+               self.params = obj.params.copy()
+          else:
+              raise ValueError("obj is not of recognized type (recognized: str,dict, StimulusID or Stimulus)")
+              
+
+
+"""
+Various common operations over StimulusID lists (and associated lists of data) follow.
+"""
+
+def _colapse(dd,param):
+    d = {}
+    for s in dd:
+        s1 = StimulusID(s)
+        
+        if param not in s1.params.keys():
+           raise KeyError('colaps: only stimuli containing parameter [%s] can be collapsed again parameter [%s]' % (param,param)) 
+        
+        s1.params[param]=None
+        s1 = str(s1)
+        if d.has_key(s1):
+           d[s1].extend(dd[s])
+        else:
+           d[s1] = dd[s]
+    return d
+    
+def colapse(data_list,stimuli_list,func=None,parameter_list=[],allow_non_identical_stimuli=False):
+    """
+    It colapses the stimuli_list and associated data_list acording to stimuli 
+    against parameters in parameter_list. This means that the new list of parameters (and associated datalist)
+    will contain one stimulus for each combination of parameter values not among the paramters against
+    which to collapse. Each such stimulus will be associated with a list of data that corresponded to 
+    stimuli with the same parameter values, but any values for the paramters against which one is collapsing.
+    The collapsed parameters in the stimuli_list will be replaced with None.
+    
+    The function returns a tuple of lists (v,stimuli_id), where stimuli_id is
+    the new list of stimuli where the stimuli in parameter_list were 'colapsed out'
+    and replaced with None. v is a list of lists. The outer list corresponding 
+    in order to the stimuli_id list. The inner list corresponds to the list of data from data_list 
+    that mapped on the given stimuli_id. 
+    
+    If func != None, func is applied to each member of v.
+    
+    data_list - the list of data corresponding to stimuli in stimuli_list
+    stimuli_list - the list of stimuli corresponding to data in data_list
+    parameter_list - the list of parameter names against which to colapse the data
+    func -       the func to be aplied to the lists formed by data associated with the 
+                 same stimuli parametrizations with exception of the parameters in parameter_list
+    allow_non_identical_stimuli - (defaul=False) unless set to True, it will not allow runing this operation on
+                                  StimulusDependentData that does not contain only stimuli of the same type
+    """
+    assert(len(data_list) == len(stimuli_list))
+    if (not allow_non_identical_stimuli) and (not identical_stimulus_type(stimuli_list)):
+       raise ValueError("colapse accepts only stimuli lists of the same type")  
+
+    
+    d = {}
+    for v,s in zip(data_list,stimuli_list):
+        d[str(s)]=[v]
+
+    for param in parameter_list:
+        d = _colapse(d,param)
+    
+    values = [d[k] for k in d.keys()] 
+    st = [StimulusID(idd) for idd in d.keys()]
+    
+    if func != None:
+       return ([func(v) for v in values],st)
+    else:
+       return (values,st)
+
+def varying_parameters(stimulus_ids):
+    """
+    Find the varying list of params. Can be only applied 
+    on a stimulus list containing identical type of stimuli.
+    """ 
+    if not identical_stimulus_type(stimuli_ids):
+       raise ValueError("varying_parameters: accepts only stimuli lists of the same type")  
+    
+    p = stimuli_ids[0].params.copy()
+    varying_params = {}
+    for n in p.keys():
+        for sid in stimuli_ids:
+            if sid.params[n] != p[n]:
+               varying_params[n] = True
+               break;
+    return varying_params
+
+
+def colapse_to_dictionary(value_list,stimuli_list,parameter_name):
+    """
+    Returns dictionary D where D.keys() correspond to stimuli_ids 
+    with the dimension 'parameter name' colapsed out (and replaced with None).
+        
+    The D.values() are tuple of lists (keys,values), where keys is the list of 
+    values that the parameter_name had in the stimuli_list, and the values are the 
+    values from value_list that correspond to the keys.
+    """
+    assert(len(value_list) == len(stimuli_list))
+    d = {}
+    
+    for (v,s) in zip(value_list,stimuli_list):
+        s=s.copy()
+        val = s.params[parameter_name]
+        s.params[parameter_name] = None
+        if d.has_key(str(s)):
+           (a,b) = d[str(s)] 
+           a.append(val)
+           b.append(v)
+        else:
+           d[str(s)]  = ([val],[v]) 
+    dd = {}
+    for k in d:
+        (a,b) = d[k]
+        dd[k] = (a,b)
+    return dd
+    
+def identical_stimulus_type(stimuli_list):
+    """
+    Returns true if all stimuli in stimulus_list are of the same type, else returns False.
+    """
+    stimulus_type = StimulusID(stimuli_list[0]).name
+    for st in stimuli_list:
+        if StimulusID(st).name != stimulus_type:
+            return False
+    return True
+
+def find_stimuli(stimulus_name,stimuli_list,data_list=None,**kwargs):
+    """
+    Returns list of stimuli (and associated data if data_list!=None) of stimulus_name type and 
+    for which the parameters in kwargs match. 
+    
+    stimulus_name - the name of the stimulus to filter out
+    data_list - the list of values corresponding to stimuli in stimuli_list
+    stimuli_list - the list of stimuli corresponding to values in value_list
+    **kwargs - the parameter names and values that have to match
+    
+    
+    """
+    new_st = []
+    new_d = []
+    
+    no_data = False
+    if data_list == None:
+       data_list = [[] for z in xrange(0,len(stimuli_list))] 
+       no_data = True
+    else:
+       assert(len(data_list) == len(stimuli_list))        
+    for sid,data in (stimuli_list, data_list):
+        sid = StimulusID(sid)
+        if sid.name == stimulus_name:
+                    flag=True    
+                    for n,f in kwargs.items():
+                        if (not sid.params.has_key(n)) or f != sid.params[n]:
+                           flag=False;
+                           break;
+                    if flag:
+                        new_st.append(sid) 
+                        new_d.append(data) 
+
+    if no_data:
+        return new_st
+    else:
+        return (new_st,new_d)
+                  
+
---
+++ mozaik/stimuli/visual_stimulus.py
@@ -0,0 +1,136 @@
+import numpy
+from operator import *
+import quantities as qt
+from mozaik.stimuli.stimulus import Stimulus
+from mozaik.framework.space import TRANSPARENT, xy2ij
+from mozaik.tools.mozaik_parametrized import SNumber, SInteger, SString
+from mozaik import __version__
+from mozaik.tools.units import lux
+from mozaik.framework.space import VisualRegion
+from mozaik.framework.interfaces import MozaikParametrizeObject
+from scipy.ndimage import interpolation
+
+class VisualStimulus(Stimulus):
+    """Abstract base class for visual stimuli."""
+    max_luminance = SNumber(lux,doc="""Maximum luminance""")
+    density = SNumber(1/(qt.degree),doc="""The density of stimulus - units per degree""")
+    location_x = SNumber(qt.degrees,doc="""x location of the center of  visual region.""")
+    location_y = SNumber(qt.degrees,doc="""y location of the center of  visual region.""")
+    size_x = SNumber(qt.degrees,doc="""The size of the region in degrees (asimuth).""")
+    size_y = SNumber(qt.degrees,doc="""The size of the region in degrees (elevation).""")
+    
+    def __init__(self,**params):
+        Stimulus.__init__(self,**params)
+        self._zoom_cache = {}
+        self.is_visible = True
+        self.region = VisualRegion(self.location_x,self.location_y,self.size_x,self.size_y)
+
+    def update(self):
+        raise NotImplementedException("Must be implemented by child class")
+
+    def _calculate_zoom(self, actual_pixel_size, desired_pixel_size):
+        """
+        Sometimes the interpolation procedure returns a new array that is too
+        small due to rounding error. This is a crude attempt to work around that.
+        """
+        zoom = actual_pixel_size/desired_pixel_size
+        for i in self.img.shape:
+            if int(zoom*i) != round(zoom*i):
+                zoom *= (1 + 1e-15)
+        return zoom
+
+    def display(self, region, pixel_size):
+        assert isinstance(region, VisualRegion), "region must be a VisualRegion-descended object. Actually a %s" % type(region)
+        size_in_pixels = numpy.ceil(xy2ij((region.size_x,region.size_y))/float(pixel_size)).astype(int)
+        view = TRANSPARENT*numpy.ones(size_in_pixels)
+        if region.overlaps(self.region):
+            intersection = region.intersection(self.region)
+            assert intersection == self.region.intersection(region) # just a consistency check. Could be removed if necessary for performance.
+            img_relative_left = (intersection.left - self.region.left)/self.region.width
+            img_relative_width = intersection.width/self.region.width
+            img_relative_top = (intersection.top - self.region.bottom)/self.region.height ##
+            img_relative_bottom = (intersection.bottom - self.region.bottom)/self.region.height
+            img_relative_height = intersection.height/self.region.height 
+            view_relative_left = (intersection.left - region.left)/region.width
+            view_relative_width = intersection.width/region.width
+            view_relative_top = (intersection.top - region.bottom)/region.height ##
+            view_relative_bottom = (intersection.bottom - region.bottom)/region.height
+            view_relative_height = intersection.height/region.height
+            
+            img_pixel_size = xy2ij((self.region.size_x,self.region.size_y))/self.img.shape # is self.size a tuple or an array?
+            assert img_pixel_size[0] == img_pixel_size[1]
+            
+            if pixel_size == img_pixel_size[0]:
+                #logger.debug("Image pixel size matches desired size (%g degrees)" % pixel_size)
+                img = self.img
+            else:
+                # note that if the image is much larger than the view region, we might save some
+                # time by not rescaling the whole image, only the part within the view region.
+                zoom = self._calculate_zoom(img_pixel_size[0], pixel_size) #img_pixel_size[0]/pixel_size
+                #logger.debug("Image pixel size (%g deg) does not match desired size (%g deg). Zooming image by a factor %g" % (img_pixel_size[0], pixel_size, zoom))
+                if zoom in self._zoom_cache:
+                    img = self._zoom_cache[zoom]
+                else:                  
+                    img = interpolation.zoom(self.img, zoom)
+                    self._zoom_cache[zoom] = img
+                
+            j_start = numpy.round(img_relative_left * img.shape[1]).astype(int)
+            delta_j = numpy.round(img_relative_width * img.shape[1]).astype(int)
+            i_start = img.shape[0] - numpy.round(img_relative_top * img.shape[0]).astype(int)
+            delta_i = numpy.round(img_relative_height * img.shape[0]).astype(int)
+            
+            l_start = numpy.round(view_relative_left * size_in_pixels[1]).astype(int) 
+            delta_l = numpy.round(view_relative_width * size_in_pixels[1]).astype(int)
+            k_start = size_in_pixels[0] - numpy.round(view_relative_top * size_in_pixels[0]).astype(int) 
+            delta_k = numpy.round(view_relative_height * size_in_pixels[0]).astype(int) 
+            
+            assert delta_j == delta_l, "delta_j = %g, delta_l = %g" % (delta_j, delta_l)
+            assert delta_i == delta_k
+            
+            i_stop = i_start + delta_i
+            j_stop = j_start + delta_j
+            k_stop = k_start + delta_k
+            l_stop = l_start + delta_l
+            ##logger.debug("i_start = %d, i_stop = %d, j_start = %d, j_stop = %d" % (i_start, i_stop, j_start, j_stop))
+            ##logger.debug("k_start = %d, k_stop = %d, l_start = %d, l_stop = %d" % (k_start, k_stop, l_start, l_stop))
+
+            
+            try:
+                view[k_start:k_start+delta_k, l_start:l_start+delta_l] = img[i_start:i_start+delta_i, j_start:j_start+delta_j]
+            except ValueError, errmsg:
+                logger.error("i_start = %d, i_stop = %d, j_start = %d, j_stop = %d" % (i_start, i_stop, j_start, j_stop))
+                logger.error("k_start = %d, k_stop = %d, l_start = %d, l_stop = %d" % (k_start, k_stop, l_start, l_stop))
+                logger.error("img.shape = %s, view.shape = %s" % (img.shape, view.shape))
+                logger.error("img[i_start:i_stop, j_start:j_stop].shape = %s" % str(img[i_start:i_stop, j_start:j_stop].shape))
+                logger.error("view[k_start:k_stop, l_start:l_stop].shape = %s" % str(view[k_start:k_stop, l_start:l_stop].shape))
+                raise
+        assert view.shape == tuple(size_in_pixels), "view.shape = %s, should be %s" % (view.shape, tuple(size_in_pixels))
+        return view
+
+
+    def update(self):
+        """
+        Sets the current frame to the next frame in the sequence.
+        """
+        try:
+            self.img, self.variables = self._frames.next()
+
+        except StopIteration:
+            self.visible = False
+        else:
+            assert self.img.min() >= 0 or self.img.min() == TRANSPARENT, "frame minimum is less than zero: %g" % self.img.min()
+            assert self.img.max() <= self.max_luminance, "frame maximum (%g) is greater than the maximum luminance (%g)" % (self.img.max(), self.max_luminance)
+        self._zoom_cache = {}
+    
+    def reset(self):
+        """
+        Reset to the first frame in the sequence.
+        """
+        self.visible = True
+        self._frames = self.frames()
+        self.update()
+
+    def next_frame(self):
+        """For creating movies with NeuroTools.visualization."""
+        self.update()
+        return [self.img]
