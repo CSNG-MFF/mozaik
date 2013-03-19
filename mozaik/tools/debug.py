fbb520b8816c33420ae9455b1ac720c52615a51a
Jan Antolik <antolikjan@gmail.com>
2013-03-19 11:28:26 +0100

    added missing files

---
+++ mozaik/tools/debug.py
@@ -0,0 +1,12 @@
+import pylab
+import pickle
+
+def visualize_retinal_cache(data_store,which):
+    f = open('./retinal_cache/' + str(which) + '.st','r')
+    cached_stimulus = pickle.load(f1)
+    z = pickle.load(f)   
+    
+    pos = data_store.get_neuron_postions()['X_ON']
+    
+    pylab.figure()
+    pylab.scatter(pos[0],pos[1],color=cached_stimulus[:,0])
