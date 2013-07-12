75714b3293681d89414f88ebab550d277c2e6d12
Jan Antolik <antolikjan@gmail.com>
2013-07-12 16:10:26 +0200

    little bugs

--- examples/VogelsAbbott2005/experiments.py
+++ examples/VogelsAbbott2005/experiments.py
@@ -4,12 +4,12 @@ from mozaik.sheets.population_selector import RCRandomPercentage
 from parameters import ParameterSet
     
 def create_experiments(model):
-    l4exc_kick = RCRandomPercentage(model.sheets["Exc_Layer"],ParameterSet({'percentage': 20.0}))
-    l4inh_kick = RCRandomPercentage(model.sheets["Inh_Layer"],ParameterSet({'percentage': 20.0}))
+    exc_kick = RCRandomPercentage(model.sheets["Exc_Layer"],ParameterSet({'percentage': 20.0}))
+    inh_kick = RCRandomPercentage(model.sheets["Inh_Layer"],ParameterSet({'percentage': 20.0}))
 
     return  [
                            #Lets kick the network up into activation
-                           PoissonNetworkKick(model,duration=8*7,sheet_list=["Exc_Layer","Inh_Layer"],recording_configuration_list=[l4exc_kick,l4inh_kick],lambda_list=[100,100]),
+                           PoissonNetworkKick(model,duration=8*7,sheet_list=["Exc_Layer","Inh_Layer"],recording_configuration_list=[exc_kick,inh_kick],lambda_list=[100,100]),
                            #Spontaneous Activity 
                            NoStimulation(model,duration=3*8*7),
             ]
