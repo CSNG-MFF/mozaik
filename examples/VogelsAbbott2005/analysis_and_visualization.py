d39579ffd93e22b24593687823f9291ba017f2e9
JoelChavas <joel.chavas@unic.cnrs-gif.fr>
2014-09-03 12:27:04 +0000

    add the spontaneous keyword in examples. Comment the OverviewPlot lines as they are not working so far. The example now runs and produces a raster plot

--- examples/VogelsAbbott2005/analysis_and_visualization.py
+++ examples/VogelsAbbott2005/analysis_and_visualization.py
@@ -32,13 +32,13 @@ def perform_analysis_and_visualization(data_store):
             
             dsv = param_filter_query(data_store,st_direct_stimulation_name=['None'])    
             
-            OverviewPlot(dsv,ParameterSet({'sheet_name' : 'Exc_Layer', 'neuron' : analog_ids[0], 'sheet_activity' : {}}),fig_param={'dpi' : 100,'figsize': (19,12)},plot_file_name='ExcAnalog1.png').plot({'Vm_plot.y_lim' : (-80,-50),'Conductance_plot.y_lim' : (0,500.0)})
-            OverviewPlot(dsv,ParameterSet({'sheet_name' : 'Exc_Layer', 'neuron' : analog_ids[1], 'sheet_activity' : {}}),fig_param={'dpi' : 100,'figsize': (19,12)},plot_file_name='ExcAnalog2.png').plot({'Vm_plot.y_lim' : (-80,-50),'Conductance_plot.y_lim' : (0,500.0)})    
-            OverviewPlot(dsv,ParameterSet({'sheet_name' : 'Exc_Layer', 'neuron' : analog_ids[2], 'sheet_activity' : {}}),fig_param={'dpi' : 100,'figsize': (19,12)},plot_file_name='ExcAnalog3.png').plot({'Vm_plot.y_lim' : (-80,-50),'Conductance_plot.y_lim' : (0,500.0)})
+            #OverviewPlot(dsv,ParameterSet({'sheet_name' : 'Exc_Layer', 'neuron' : analog_ids[0], 'sheet_activity' : {}}),fig_param={'dpi' : 100,'figsize': (19,12)},plot_file_name='ExcAnalog1.png').plot({'Vm_plot.y_lim' : (-80,-50),'Conductance_plot.y_lim' : (0,500.0)})
+            #OverviewPlot(dsv,ParameterSet({'sheet_name' : 'Exc_Layer', 'neuron' : analog_ids[1], 'sheet_activity' : {}}),fig_param={'dpi' : 100,'figsize': (19,12)},plot_file_name='ExcAnalog2.png').plot({'Vm_plot.y_lim' : (-80,-50),'Conductance_plot.y_lim' : (0,500.0)})    
+            #OverviewPlot(dsv,ParameterSet({'sheet_name' : 'Exc_Layer', 'neuron' : analog_ids[2], 'sheet_activity' : {}}),fig_param={'dpi' : 100,'figsize': (19,12)},plot_file_name='ExcAnalog3.png').plot({'Vm_plot.y_lim' : (-80,-50),'Conductance_plot.y_lim' : (0,500.0)})
             
             
-            RasterPlot(dsv,ParameterSet({'sheet_name' : 'Exc_Layer', 'neurons' : spike_ids,'trial_averaged_histogram': False}),fig_param={'dpi' : 100,'figsize': (17,5)},plot_file_name='ExcRaster.png').plot({'SpikeRasterPlot.group_trials':True})
-            RasterPlot(dsv,ParameterSet({'sheet_name' : 'Inh_Layer', 'neurons' : spike_ids_inh,'trial_averaged_histogram': False}),fig_param={'dpi' : 100,'figsize': (17,5)},plot_file_name='InhRaster.png').plot({'SpikeRasterPlot.group_trials':True})
+            RasterPlot(dsv,ParameterSet({'sheet_name' : 'Exc_Layer', 'neurons' : spike_ids,'trial_averaged_histogram': False, 'spontaneous': False}),fig_param={'dpi' : 100,'figsize': (17,5)},plot_file_name='ExcRaster.png').plot({'SpikeRasterPlot.group_trials':True})
+            RasterPlot(dsv,ParameterSet({'sheet_name' : 'Inh_Layer', 'neurons' : spike_ids_inh,'trial_averaged_histogram': False, 'spontaneous': False}),fig_param={'dpi' : 100,'figsize': (17,5)},plot_file_name='InhRaster.png').plot({'SpikeRasterPlot.group_trials':True})
 
             import pylab
             pylab.show()
