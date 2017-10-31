a99d04cc74e917afbf35f6814c2f1c0c2acc7e7b
Jan Antolik <antolikjan@gmail.com>
2017-10-31 11:53:14 +0100

    bug correction

--- mozaik/meta_workflow/analysis.py
+++ mozaik/meta_workflow/analysis.py
@@ -149,7 +149,8 @@ def export_SingleValues_as_matricies(simulation_name,master_results_dir,query):
         for (pv,datastore) in datastores:
             index = [param_values[i].index(pv[i]) for i in xrange(0,len(param_values))]
             dsv = query.query(datastore)
-            vv = param_filter_query(dsv,identifier='SingleValue',value_name=v).get_analysis_result()[0].value
+            if len(param_filter_query(dsv,identifier='SingleValue',value_name=v).get_analysis_result()) == 1:
+                vv = param_filter_query(dsv,identifier='SingleValue',value_name=v).get_analysis_result()[0].value
             matrix[tuple(index)] = vv
         f = open(v+'.txt','w')
         pickle.dump((parameters,param_values,matrix),f)
