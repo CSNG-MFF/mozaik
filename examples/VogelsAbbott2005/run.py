7fca9c6009ba77b276555f14143af9d7826013cd
Vit Fojtik <fojtik.vit@gmail.com>
2018-11-14 10:52:33 +0100

    removed MPI from example

--- examples/VogelsAbbott2005/run.py
+++ examples/VogelsAbbott2005/run.py
@@ -11,7 +11,7 @@ mpirun python run.py simulator_name number_processors parameters name_of_test
 For example:
 mpirun python run.py nest 2 param/defaults 'test'
 """
-from mpi4py import MPI 
+#from mpi4py import MPI 
 from pyNN import nest
 import sys
 import mozaik
@@ -22,7 +22,7 @@ from mozaik.storage.datastore import Hdf5DataStore,PickledDataStore
 from analysis_and_visualization import perform_analysis_and_visualization
 from parameters import ParameterSet
 
-mpi_comm = MPI.COMM_WORLD
+#mpi_comm = MPI.COMM_WORLD
 logger = mozaik.getMozaikLogger()
 
 if True:
@@ -32,7 +32,7 @@ else:
     data_store = PickledDataStore(load=True,parameters=ParameterSet({'root_directory':'VogelsAbbott2005_test_____', 'store_stimuli' : False}),replace=True)
     logger.info('Loaded data store')
 
-if mpi_comm.rank == 0:
-   print "Starting visualization" 
-   perform_analysis_and_visualization(data_store)
-   data_store.save() 
+#if mpi_comm.rank == 0:
+print "Starting visualization" 
+perform_analysis_and_visualization(data_store)
+data_store.save() 
