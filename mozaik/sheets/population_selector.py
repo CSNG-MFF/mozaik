52b8ce9da9fb5c40ddf41d7eb2df3b70b095a077
Racagnol <racagnol@gmail.com>
2020-11-30 17:51:01 +0100

    Clean up of the setup.py file and litle correction in the population_selector code

--- mozaik/sheets/population_selector.py
+++ mozaik/sheets/population_selector.py
@@ -234,10 +234,7 @@ class SimilarAnnotationSelectorRegion(SimilarAnnotationSelector):
       def generate_idd_list_of_neurons(self):
           picked_or = set(self.pick_close_to_annotation())
           xx,yy = self.sheet.cs_2_vf(self.sheet.pop.positions[0],self.sheet.pop.positions[1])
-          picked_region = set(numpy.arange(0,len(xx))[numpy.logical_and((
-              abs(numpy.array(xx - self.parameters.offset_x) < self.parameters.size/2.0),
-              abs(numpy.array(yy - self.parameters.offset_y) < self.parameters.size/2.0)
-          ))])
+          picked_region = set(numpy.arange(0,len(xx))[numpy.logical_and(abs(numpy.array(xx - self.parameters.offset_x) < self.parameters.size/2.0) , (abs(numpy.array(yy) - self.parameters.offset_y) < self.parameters.size/2.0))])
           picked = list(picked_or & picked_region)  
           mozaik.rng.shuffle(picked)
           z = self.sheet.pop.all_cells.astype(int)
