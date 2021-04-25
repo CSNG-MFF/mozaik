a835824462edec6bad0acac70cc0defc2360e40d
Jan Antolik <antolikjan@gmail.com>
2021-04-25 17:31:41 +0200

    minor changes

--- mozaik/models/vision/spatiotemporalfilter.py
+++ mozaik/models/vision/spatiotemporalfilter.py
@@ -631,6 +631,7 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
                                               self.parameters.gain_control,visual_space)
             input_cells[rf_type].initialize(visual_space.background_luminance, duration)
         
+
         for rf_type in self.rf_types:
 
                 if self.parameters.gain_control.non_linear_gain != None:
@@ -638,11 +639,24 @@ class SpatioTemporalFilterRetinaLGN(SensoryInputComponent):
                 else:
                         amplitude = self.parameters.linear_scaler * self.parameters.gain_control.gain * numpy.sum(input_cells[rf_type].receptive_field.kernel.flatten())*visual_space.background_luminance
 
+                logger.info("null input times: " + str(self.parameters.linear_scaler))
+
+                logger.info("null input times: " + str(self.parameters.gain_control.non_linear_gain.luminance_gain))
+                logger.info("null input times: " + str(self.parameters.linear_scaler))
+                logger.info("null input times: " + str(visual_space.background_luminance))
+                logger.info("null input times: " + str((self.parameters.gain_control.non_linear_gain.luminance_scaler*visual_space.background_luminance+1.0)
+))
+                logger.info("null input times: " + str(visual_space.background_luminance/(self.parameters.gain_control.non_linear_gain.luminance_scaler*visual_space.background_luminance+1.0)
+))
+                logger.info("null input amplitude: " + str(numpy.sum(input_cells[rf_type].receptive_field.kernel.flatten())*visual_space.background_luminance / (self.parameters.gain_control.non_linear_gain.luminance_scaler*visual_space.background_luminance+1.0)
+))
+                logger.info("null input times: " + str(times))
+                logger.info("null input amplitude: " + str(amplitude))
+
                 for i, (scs, ncs) in enumerate(zip(self.scs[rf_type],self.ncs[rf_type])):
                     scs.set_parameters(times=times,amplitudes=zers+amplitude,copy=False)
                     if self.parameters.mpi_reproducible_noise:
                         t = numpy.arange(0, duration, ts) + offset
-
                         amplitudes = (self.parameters.noise.mean
                                         + self.parameters.noise.stdev
                                            * self.ncs_rng[rf_type][i].randn(len(t)))
