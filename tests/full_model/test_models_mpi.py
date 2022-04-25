5ef22b4f4fe5a838fd4408425d0758ef7d78d501
RCagnol <racagnol@gmail.com>
2022-04-25 12:48:25 +0200

    Correction in the mpi test

--- tests/full_model/test_models_mpi.py
+++ tests/full_model/test_models_mpi.py
@@ -42,4 +42,4 @@ class TestLSV1MTiny(TestModel):
         "sheet_name", ["V1_Exc_L4", "V1_Inh_L4", "V1_Exc_L2/3", "V1_Inh_L2/3"]
     )
     def test_voltages(self, sheet_name):
-
+         self.check_voltages(self.ds, self.ds_ref, sheet_name, max_neurons=25)
