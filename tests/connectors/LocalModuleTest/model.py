462e4f6071d779fcb58464b17faf80159bdab6ff
RCagnol <racagnol@gmail.com>
2022-05-27 12:04:06 +0200

    Adding tests for local module

---
+++ tests/connectors/LocalModuleTest/model.py
@@ -0,0 +1,47 @@
+import sys
+from parameters import ParameterSet
+from mozaik.models import Model
+from mozaik.connectors.meta_connectors import GaborConnector
+from mozaik.connectors.modular import (
+    ModularSamplingProbabilisticConnector,
+    ModularSamplingProbabilisticConnectorAnnotationSamplesCount,
+)
+from mozaik import load_component
+from mozaik.space import VisualRegion
+import mozaik
+
+
+class ModelLocalModule(Model):
+
+    required_parameters = ParameterSet(
+        {
+            "sheets": ParameterSet(
+                {
+                    "sheet": ParameterSet,
+                }
+            ),
+        }
+    )
+
+    def __init__(self, sim, num_threads, parameters):
+        Model.__init__(self, sim, num_threads, parameters)
+        # Load components
+        Sheet = load_component(self.parameters.sheets.sheet.component)
+        Sheet_lm = load_component(self.parameters.sheets.sheet_lm.component)
+
+        sheet = Sheet(self, self.parameters.sheets.sheet.params)
+        sheet_lm = Sheet_lm(self, self.parameters.sheets.sheet_lm.params)
+        ModularSamplingProbabilisticConnector(
+            self,
+            "RecurrentConnection",
+            sheet,
+            sheet,
+            self.parameters.sheets.sheet.RecurrentConnection,
+        ).connect()
+        ModularSamplingProbabilisticConnector(
+            self,
+            "RecurrentConnectionLM",
+            sheet_lm,
+            sheet_lm,
+            self.parameters.sheets.sheet_lm.RecurrentConnectionLM,
+        ).connect()
