5b48be64cc0ce888a3570d25680243eaff03a408
RCagnol <racagnol@gmail.com>
2022-08-10 16:52:03 +0200

    Reformating the files

--- tests/parametrization/test_parametrization.py
+++ tests/parametrization/test_parametrization.py
@@ -7,10 +7,12 @@ import os
 from collections import OrderedDict
 import pytest
 
+
 class TestParametrization:
     """
     Tests for the TestParametrization class
     """
+
     def setup_class(cls):
         os.chdir("tests/parametrization/")
 
@@ -18,14 +20,17 @@ class TestParametrization:
         p["mozaik_seed"] = 1023
         p["pynn_seed"] = 936395
         mozaik.setup_mpi(**p)
-        cls.param_original_PyNNDistribution = load_parameters('param_original_PyNNDistribution/defaults')
+        cls.param_original_PyNNDistribution = load_parameters(
+            "param_original_PyNNDistribution/defaults"
+        )
 
         mozaik.setup_mpi(**p)
-        cls.param_parametrized_PyNNDistribution = load_parameters('param_parametrized_PyNNDistribution/defaults')
+        cls.param_parametrized_PyNNDistribution = load_parameters(
+            "param_parametrized_PyNNDistribution/defaults"
+        )
 
     def test_parametrization_PyNNDistribution(self):
         assert (
-            self.param_original_PyNNDistribution.num_samples.next() == self.param_parametrized_PyNNDistribution.num_samples.next()
+            self.param_original_PyNNDistribution.num_samples.next()
+            == self.param_parametrized_PyNNDistribution.num_samples.next()
         ), "num_samples of the two parametrization generate different numbers"
-
-
