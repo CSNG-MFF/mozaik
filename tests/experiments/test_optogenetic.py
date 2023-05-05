3adf542096d12a545f96035ccfcab819cb398083
RCagnol <racagnol@gmail.com>
2023-05-05 18:41:36 +0200

    Formatting correction

--- tests/experiments/test_optogenetic.py
+++ tests/experiments/test_optogenetic.py
@@ -246,7 +246,7 @@ class TestOptogeneticArrayImageStimulus(TestCorticalStimulationWithOptogeneticAr
             ParameterSet(
                 {
                     "map_location": test_dir + "/sheets/or_map",
-                    "map_stretch" : 1,
+                    "map_stretch": 1,
                     "sigma": 0,
                     "periodic": True,
                 }
@@ -278,7 +278,7 @@ class TestOptogeneticArrayImageStimulus(TestCorticalStimulationWithOptogeneticAr
             ParameterSet(
                 {
                     "map_location": test_dir + "/sheets/or_map",
-                    "map_stretch" : 1,
+                    "map_stretch": 1,
                     "sigma": 0,
                     "periodic": True,
                 }
@@ -328,7 +328,12 @@ class TestOptogeneticArrayStimulusOrientationTuningProtocol(
             self.sheet,
             self.sheet,
             ParameterSet(
-                {"map_location": "tests/sheets/or_map", "map_stretch" : 1, "sigma": 0, "periodic": True}
+                {
+                    "map_location": "tests/sheets/or_map",
+                    "map_stretch": 1,
+                    "sigma": 0,
+                    "periodic": True,
+                }
             ),
         )
 
