599415d968f968d5ceb987d385b7e14021e66d25
RCagnol <racagnol@gmail.com>
2023-06-06 17:54:08 +0200

    Separating the rng test into two tests, one invoking 2 MPI processes and that can be ran through github actions, one invoking 7 processes and that has to be ran locally

--- tests/full_model/test_models_mpi.py
+++ tests/full_model/test_models_mpi.py
@@ -14,14 +14,14 @@ import pytest
 import mozaik
 
 
-class TestLSV1MTiny(TestModel):
+class TestLSV1MTinyMPI(TestModel):
     """
     Class that runs the a tiny version of the LSV1M model on construction from the mozaik-models
-    repository. Its testing methods compare the membrane potentials of a few neurons and the
-    spike times of all neurons to a saved reference.
+    repository and runs it with MPI. Its testing methods compare the membrane potentials of a
+    few neurons and the spike times of all neurons to a saved reference.
     """
 
-    model_run_command = "cd tests/full_model/models/LSV1M_tiny && mpirun -np 7 python run.py nest 1 param/defaults 'pytest' && cd ../../../.."
+    model_run_command = "cd tests/full_model/models/LSV1M_tiny && mpirun -np 2 python run.py nest 1 param/defaults 'pytest' && cd ../../../.."
     result_path = "tests/full_model/models/LSV1M_tiny/LSV1M_pytest_____"
     ref_path = "tests/full_model/reference_data/LSV1M_tiny_mpi"
 
@@ -46,7 +46,29 @@ class TestLSV1MTiny(TestModel):
 
     @pytest.mark.model
     @pytest.mark.mpi
-    def test_mozaik_rng(self):
+    def test_mozaik_rng_mpi2(self):
+        rngs_state = self.ds.block.annotations["simulation_log"]["rngs_state"]
+        print(rngs_state)
+        assert len(set(rngs_state)) == 1
+
+class TestLSV1MTinyMPI7(TestModel):
+    """
+    Class that runs the a tiny version of the LSV1M model on construction from the mozaik-models
+    repository and runs it with MPI using 7 processes. Its testing methods compare the membrane
+    potentials of a few neurons and the spike times of all neurons to a saved reference.
+    """
+
+    model_run_command = "cd tests/full_model/models/LSV1M_tiny && mpirun -np 7 python run.py nest 1 param/defaults 'pytest' && cd ../../../.."
+    result_path = "tests/full_model/models/LSV1M_tiny/LSV1M_pytest_____"
+    ref_path = "tests/full_model/reference_data/LSV1M_tiny_mpi"
+
+    ds = None  # Model run datastore
+    ds_ref = None  # Reference datastore
+
+    @pytest.mark.model
+    @pytest.mark.mpi
+    @pytest.mark.not_github
+    def test_mozaik_rng_mpi7(self):
         rngs_state = self.ds.block.annotations["simulation_log"]["rngs_state"]
         print(rngs_state)
         assert len(set(rngs_state)) == 1
