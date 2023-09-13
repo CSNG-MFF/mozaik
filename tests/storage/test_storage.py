541dd1de48f31ee8864d06e06b74a63e073e14fe
RCagnol <racagnol@gmail.com>
2023-09-13 18:02:20 +0200

    Reformatting

--- tests/storage/test_storage.py
+++ tests/storage/test_storage.py
@@ -1,9 +1,9 @@
 import matplotlib
 
 matplotlib.use("Agg")
-from mozaik.analysis.analysis import * 
-from mozaik.storage.datastore import * 
-from mozaik.storage.queries import param_filter_query 
+from mozaik.analysis.analysis import *
+from mozaik.storage.datastore import *
+from mozaik.storage.queries import param_filter_query
 import mozaik
 import logging
 import os
@@ -21,7 +21,10 @@ class TestDatastore:
         Runs the model and loads its result and a saved reference result
         """
         cls.ds = cls.load_datastore(cls.ref_path)
-        TrialAveragedFiringRate(param_filter_query(cls.ds,st_name='FullfieldDriftingSinusoidalGrating'), ParameterSet({})).analyse()
+        TrialAveragedFiringRate(
+            param_filter_query(cls.ds, st_name="FullfieldDriftingSinusoidalGrating"),
+            ParameterSet({}),
+        ).analyse()
         cls.ads = cls.ds.get_analysis_result()
 
     @staticmethod
@@ -44,60 +47,74 @@ class TestDatastore:
             ),
             replace=False,
         )
+
     def test_ADS_sorting_homogeneity(self):
-        dsv_sorted = param_filter_query(self.ds,st_name='FullfieldDriftingSinusoidalGrating')
-        dsv_sorted.sort_analysis_results('sheet_name')
+        dsv_sorted = param_filter_query(
+            self.ds, st_name="FullfieldDriftingSinusoidalGrating"
+        )
+        dsv_sorted.sort_analysis_results("sheet_name")
         adss_sorted = dsv_sorted.get_analysis_result()
         count_incorrect = 0
         for ads in adss_sorted:
             if ads not in self.ads:
-                count_incorrect +=1
+                count_incorrect += 1
         for ads in self.ads:
             if ads not in adss_sorted:
-                count_incorrect +=1
-        assert count_incorrect == 0 
+                count_incorrect += 1
+        assert count_incorrect == 0
+
     pass
 
     def test_ADS_sorting_st_homogeneity(self):
         dsv_sorted = param_filter_query(self.ds)
-        dsv_sorted.sort_analysis_results('st_orientation')
+        dsv_sorted.sort_analysis_results("st_orientation")
         adss_sorted = dsv_sorted.get_analysis_result()
         count_incorrect = 0
         for ads in adss_sorted:
             if ads not in self.ads:
-                count_incorrect +=1
+                count_incorrect += 1
         for ads in self.ads:
             if ads not in adss_sorted:
-                count_incorrect +=1
+                count_incorrect += 1
         assert count_incorrect == 0
+
     pass
 
     def test_ADS_sorting_order(self):
         dsv_sorted = param_filter_query(self.ds)
-        dsv_sorted.sort_analysis_results('sheet_name')
+        dsv_sorted.sort_analysis_results("sheet_name")
         adss_sorted = dsv_sorted.get_analysis_result()
         count_correct = 0
-        for ads,ads_next in zip(adss_sorted[:-1],adss_sorted[1:]):
-            if not hasattr(ads,'sheet_name'):
+        for ads, ads_next in zip(adss_sorted[:-1], adss_sorted[1:]):
+            if not hasattr(ads, "sheet_name"):
                 count_correct += 1
-            elif hasattr(ads_next,'sheet_name') and getattr(ads,'sheet_name') <= getattr(ads_next,'sheet_name'):
-                    count_correct +=1
+            elif hasattr(ads_next, "sheet_name") and getattr(
+                ads, "sheet_name"
+            ) <= getattr(ads_next, "sheet_name"):
+                count_correct += 1
         assert count_correct + 1 == len(adss_sorted)
-    pass
 
+    pass
 
     def test_ADS_sorting_st_order(self):
         dsv_sorted = param_filter_query(self.ds)
-        dsv_sorted.sort_analysis_results('st_orientation')
+        dsv_sorted.sort_analysis_results("st_orientation")
         adss_sorted = dsv_sorted.get_analysis_result()
         count_correct = 0
-        for ads,ads_next in zip(adss_sorted[:-1],adss_sorted[1:]):
+        for ads, ads_next in zip(adss_sorted[:-1], adss_sorted[1:]):
             if ads.stimulus_id == None:
                 count_correct += 1
-            elif ads_next.stimulus_id and not hasattr(ads.stimulus_id,'st_orientation'):
+            elif ads_next.stimulus_id and not hasattr(
+                ads.stimulus_id, "st_orientation"
+            ):
                 count_correct += 1
-            elif ads_next.stimulus_id and hasattr(ads_next.stimulus_id,'st_orientation'):
-                if getattr(MozaikParametrized.idd(ads.stimulus_id),'st_orientation') <= getattr(MozaikParametrized.idd(ads_next),'st_orientation'):
-                    count_correct +=1
+            elif ads_next.stimulus_id and hasattr(
+                ads_next.stimulus_id, "st_orientation"
+            ):
+                if getattr(
+                    MozaikParametrized.idd(ads.stimulus_id), "st_orientation"
+                ) <= getattr(MozaikParametrized.idd(ads_next), "st_orientation"):
+                    count_correct += 1
         assert count_correct + 1 == len(adss_sorted)
+
     pass
