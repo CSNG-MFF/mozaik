050df353f24cdee71e895301a59fd18b0fe0de0c
RCagnol <racagnol@gmail.com>
2025-06-18 17:14:10 +0200

    Adding analyses updates for Rozsa et al. 2024

--- mozaik/tools/stats.py
+++ mozaik/tools/stats.py
@@ -1,13 +1,17 @@
 import numpy
 import scipy.stats
 
-def confidence_interval(data, confidence = 0.95):
+def confidence_interval(data, confidence=0.95):
     mean = numpy.mean(data)
-    std = numpy.std(data,ddof=1)
     n = len(data)
-    confidence_interval = scipy.stats.t.interval(confidence, n-1,mean, scale=std / numpy.sqrt(n))
-    
+    sem = scipy.stats.sem(data) 
+    confidence_interval = scipy.stats.t.interval(confidence, n-1,mean, scale=sem)
     return mean, *confidence_interval
+
+def prediction_interval(data,confidence=0.95):
+    n = len(data)
+    t_critical = scipy.stats.t.ppf((1 + confidence) / 2, df=n-1)
+    return t_critical * numpy.std(data,ddof=1) * numpy.sqrt(1+1/n)
 
 def explained_variance(distribution,fit):
     return 1-numpy.sum((fit-distribution)**2)/numpy.sum((distribution-numpy.mean(distribution))**2)
