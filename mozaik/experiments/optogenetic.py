0c7f2f02b18e531930f8a46c93ed131e1d686cd7
Tibor Rozsa <tibor.rozsa@tutanota.com>
2025-09-02 16:17:54 +0200

    Add generate stimuli function required to run visual-optogenetic experiments

--- mozaik/experiments/optogenetic.py
+++ mozaik/experiments/optogenetic.py
@@ -1064,6 +1064,9 @@ class OptogeneticArrayStimulusCircleWithFullfieldSquareGrating(VisualExperiment)
                         }
                         self.append_direct_stim(model, sap, stim_params)
 
+    def generate_stimuli(self):
+        pass # We generate the stimuli later in the init function
+
     # TODO: Revise this function in CorticalStimulationWithOptogeneticArray to be more flexible
     def append_direct_stim(
         self, model, stimulator_array_parameters, stimulus_parameters
